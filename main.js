395393a6b6c29c314f1e7fac260ba320a893a911
RoshanCD2007 <81854107+RoshanCD2007@users.noreply.github.com>
2021-08-14 19:31:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+number1 = Number(document.getElementById("question").value);
+number2 = Number(document.getElementById("question2").value);
+
+function check(){
+    if("mySelect" == "Addition"){
+        number3 = number1 + number2;
+        document.getElementById("answer").innerHTML = number3;
+    }
+}
+
+
+
+
+ 
+
+
+
